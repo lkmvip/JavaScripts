6174be67a8566a25917b4e0a023ebc05df2d77b0
Mark Ronson <48460952+HopeTF@users.noreply.github.com>
2020-03-02 00:30:26 +0800

    Quanx.js

---
+++ Quanx/quanx.js
@@ -0,0 +1,9 @@
+﻿#quanxremote 1.0.3
+;^https:\/\/(raw.githubusercontent|\w+\.github)\.(com|io)\/.*\.js$ url script-response-body quanx.js
+
+Qx Mitm:  raw.githubusercontent.com
+
+var body = $response.body;
+body = '\/*\n@supported 自己的设备ID\n*\/\n' + body;
+
+$done(body);
