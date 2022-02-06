81b7056bef3fe217c67f232c5afb2c2df94a2dd3
lkmvip <12974141+lkmvip@users.noreply.github.com>
2022-02-06 11:37:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ IPA 文件安装器
 作者联系：https://t.me/axel_burks
 */
 
-var port_number = 80
+var port_number = 8080
 var plist_url = "itms-services://?action=download-manifest&url=https://raw.githubusercontent.com/lkmvip/JavaScripts/Pv/universal_jsbox.plist"
 
 // 从应用内启动
